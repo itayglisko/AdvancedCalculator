792cbf394bc85292509b7c07cb140640953dadce
Your Name <OmegaCollegeUser123@gmail.com>
2024-01-05 18:32:07 +0200

    Division Func

--- operations.py
+++ operations.py
@@ -20,3 +20,12 @@ def mul(num1: float, num2: float) -> float:
         return float(num1 * num2)
     except (ValueError, TypeError) as err:
         print("this functions needs to get an integer or a float type")
+
+
+def div(num1: float, num2: float) -> float:
+    try:
+        return float(num1 / num2)
+    except (ValueError, TypeError) as err:
+        print("this function needs to get an integer or a float type")
+    except ArithmeticError as e:
+        print(e)
