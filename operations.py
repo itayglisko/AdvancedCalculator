7eca14a08921563cd0805387d217a6d1364b6148
Your Name <OmegaCollegeUser123@gmail.com>
2024-01-05 18:28:12 +0200

    Multiply Func

--- operations.py
+++ operations.py
@@ -1,3 +1,6 @@
+from math import pow
+
+
 def add(num1: float, num2: float) -> float:
     try:
         return float(num1 + num2)
@@ -10,3 +13,10 @@ def sub(num1: float, num2: float) -> float:
         return float(num1 - num2)
     except (ValueError, TypeError) as err:
         print("this function needs to get an integer or a float type")
+
+
+def mul(num1: float, num2: float) -> float:
+    try:
+        return float(num1 * num2)
+    except (ValueError, TypeError) as err:
+        print("this functions needs to get an integer or a float type")
