989ca3743d30876ea12bf0624b70348b1ee958d8
Your Name <OmegaCollegeUser123@gmail.com>
2024-01-06 18:55:12 +0200

    Bracket function

--- utility_functions.py
+++ utility_functions.py
@@ -1,8 +1,10 @@
-#this methos is not finished
+# this method is not finished
 def isValid(lst: list[str]) -> bool:
     if alpha(lst):
         return False
     elif not Operator(lst):
+        return False
+    elif not brackets(lst):
         return False
     return True
 
@@ -19,24 +21,23 @@ def alpha(lst: list[str]) -> bool:
     return False
 
 
-
 def Operator(lst: list[str]) -> bool:
     """
     a function that checks if all the operators in the list r legal
     :param lst: a list full of the input from the user
     :return: true or false
     """
-    for charachter in lst:
-        if charachter.isdigit():
+    for character in lst:
+        if character.isdigit():
             continue
-        elif charachter == '(' or charachter == ')':
+        elif character == '(' or character == ')' or character == '.':
             continue
-        elif not ValidKey(charachter):
+        elif not validkey(character):
             return False
     return True
 
 
-def ValidKey(ch: str) -> bool:
+def validkey(ch: str) -> bool:
     """
     checks if the ch is a valid operator
     :param ch: - a char from the input
@@ -46,3 +47,31 @@ def ValidKey(ch: str) -> bool:
         case '+' | '-' | '*' | '/' | '^' | '@' | '$' | '&' | '%' | '~' | '!':
             return True
     return False
+
+def brackets(lst: list[str]) -> bool:
+    """
+    checks if there is a closedbracket for each opened bracket
+    and remove empty brackets from the list
+    :param lst:  full of the input from the user
+    :return: true or false
+    """
+    openbracket = 0
+    closebracket = 0
+    for index, character in enumerate(lst):
+        if character == '(':
+            openbracket += 1
+            idx = index
+        elif character == ')':
+            closebracket += 1
+            if idx + 1 == index:
+                del lst[idx:index+1]
+    if openbracket != closebracket:
+        return False
+    return True
+
+def Cast(lst: list[str]) -> list:
+    """
+    gets with a correct input and convert the numbers into float
+    :param lst: list full of the input's user
+    :return: a list
+    """
