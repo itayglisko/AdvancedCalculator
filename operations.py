db5ec4e010f4281c4b00a61c472fc9e2f0560652
Your Name <OmegaCollegeUser123@gmail.com>
2024-01-16 19:53:38 +0200

    adding documentation to all the functions

--- operations.py
+++ operations.py
@@ -4,18 +4,42 @@ from math import pow
 class Mymath:
     @staticmethod
     def add(num1: float, num2: float) -> float:
+        """
+        calculate num1 + num2
+        :param num1: A parameter that is involved on the operation
+        :param num2: A parameter that is involved on the operation
+        :return: the result from the operation
+        """
         return float(num1 + num2)
 
     @staticmethod
     def sub(num1: float, num2: float) -> float:
+        """
+        calculate the subtraction between num1 and num2
+        :param num1: A parameter that is involved on the operation
+        :param num2: A parameter that is involved on the operation
+        :return: the result from the operation
+        """
         return float(num1 - num2)
 
     @staticmethod
     def mul(num1: float, num2: float) -> float:
+        """
+        calculate nun1 * num2
+        :param num1: A parameter that is involved on the operation
+        :param num2: A parameter that is involved on the operation
+        :return: the result from the operation
+        """
         return float(num1 * num2)
 
     @staticmethod
     def div(num1: float, num2: float) -> float:
+        """
+        calculate num1/num2
+        :param num1: A parameter that is involved on the operation
+        :param num2: A parameter that is involved on the operation
+        :return: the result from the operation
+        """
         try:
             return float(num1 / num2)
         except ArithmeticError as e:
@@ -23,55 +47,98 @@ class Mymath:
 
     @staticmethod
     def pow(num1: float, num2: float) -> float:
+        """
+        calculate num1 to the power of num2 (num1^num2)
+        :param num1: A parameter that is involved on the operation
+        :param num2: A parameter that is involved on the operation
+        :return: the result from the operation
+        :raise: ArithmeticError if the 2 parameters are 0
+        """
+        if num1 == 0 and num2 == 0:
+            raise ArithmeticError("Math Error 0^0 is undefined")
         return pow(num1, num2)
 
     @staticmethod
     def avg(num1: float, num2: float) -> float:
+        """
+        calculate the average between num1 and num2
+        :param num1: A parameter that is involved on the operation
+        :param num2: A parameter that is involved on the operation
+        :return: the result from the operation
+        """
         return (num1 + num2) / 2
 
     @staticmethod
     def max(num1: float, num2: float) -> float:
+        """
+        choosing the bigger number between num1 and num2
+        :param num1: A parameter that is involved on the operation
+        :param num2: A parameter that is involved on the operation
+        :return: the result from the operation
+        """
         return num1 if num1 > num2 else num2
 
     @staticmethod
     def min(num1: float, num2: float) -> float:
+        """
+        choosing the lower number between num1 and num2
+        :param num1: A parameter that is involved on the operation
+        :param num2: A parameter that is involved on the operation
+        :return: the result from the operation
+        """
         return num1 if num1 < num2 else num2
 
     @staticmethod
     def reminder(num1: float, num2: float) -> float:
+        """
+        calculate the reminder from the operation num1/num2
+        :param num1: A parameter that is involved on the operation
+        :param num2: A parameter that is involved on the operation
+        :return: the result from the operation
+        """
         return num1 % num2
 
     @staticmethod
     def tilda(num: float) -> float:
+        """
+        making num to be the opposite number of it
+        :param num: A parameter that is involved on the operation
+        :return: the result from the operation
+        """
         return -num
 
     @staticmethod
     def factorial(num: float) -> float:
+        """
+        calculate the factorial of the parameter
+        :param num: A parameter that is involved on the operation
+        :return: the result from the operation
+        :raise: ArithmeticError if the parameter is either negative or float
+        """
+        if not int(num) == num:
+            raise ArithmeticError("Factorial is not defined for float numbers.")
         if num < 0:
-            raise ValueError("Factorial is not defined for negative numbers.")
+            raise ArithmeticError("Factorial is not defined for negative numbers.")
         elif num == 0:
             return 1
         return Mymath.factorial(num - 1) * num
 
     @staticmethod
     def hashtag(num: float) -> float:
+        """
+        summing up all the digits of the parameter num
+        :param num: A parameter that is involved on the operation
+        :return: the result from the operation
+        """
         sum = 0
         flag = True
-        wasminus = False
         count = 0
         if num < 0:
-            wasminus = True
+            raise ValueError("can not calculate hashtag on a negative number")
+        else:
             num = -num
         num = str(num)
-        while flag:
-            for digit in num:
-                if digit.isdigit():
-                    sum += int(digit)
-            if sum >= 10:
-                num = str(sum)
-                sum = 0
-            else:
-                flag = False
-        if wasminus:
-            return -sum
+        for digit in num:
+            if digit.isdigit():
+                sum += int(digit)
         return sum
