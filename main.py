05d642201f88c1fa8653d297751ed3892a0d8a50
Your Name <OmegaCollegeUser123@gmail.com>
2024-01-17 21:05:05 +0200

    maked the main module look cleaner

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
-
-from operations import Mymath
 from utility_functions import is_valid, cast, add_brackets
 from calculate import calculate
 
-if __name__ == '__main__':
+
+def remove_white_spaces(equation: str) -> list:
+    """
+    checks if there are white spaces and if so then it does not append them to the list,
+    this function creates a list full of the user's input while ignoring the white spaces
+    :param equation: the user's input
+    :return: a list without the white spaces
+    """
+    lst = []
+    for character in equation:
+        if character == ' ' or character == '\t':
+            continue
+        lst.append(character)
+    return lst
+
+
+def main():
     lst = []
     try:
         equation = input("enter a mathematical equation: ")
-    except EOFError as err:
+    except (EOFError, KeyboardInterrupt) as err:
         print(err)
     else:
-        for character in equation:
-            if character == ' ' or character == '\t':
-                continue
-            lst.append(character)
-        if is_valid(lst):
-            lst = cast(lst)
-            if lst[0] != '(':
+        lst = remove_white_spaces(equation)
+        try:
+            if is_valid(lst):
+                lst = cast(lst)
                 lst = add_brackets(lst)
-            print(lst)
-            try:
-                print(calculate(lst))
-            except (SyntaxError, OverflowError) as e:
-                print(e)
+                print(lst)
+                try:
+                    print(calculate(lst))
+                except (SyntaxError, OverflowError, KeyboardInterrupt, RecursionError, ValueError, ArithmeticError) as e:
+                    print(e)
+        except SyntaxError as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    main()
